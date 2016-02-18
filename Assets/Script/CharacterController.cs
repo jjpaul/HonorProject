5839a4c166af56cb7f39a98511898ecabb1a02fe
jjpaul <chun.k.pang@students.plymouth.ac.uk>
2016-02-18 16:32:21 +0000

    Tech demo
    little test for 2d collider and character controller

---
+++ Assets/Script/CharacterController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    public float speed = 4.0f;
+    public float jumpSpeed = 3.0f;
+
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundradius = 0.2f;
+    public LayerMask whatIsGround;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	void FixedUpdate () {
+
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundradius, whatIsGround);
+
+        float move = Input.GetAxis("Horizontal");
+
+        GetComponent<Rigidbody2D>().velocity = new Vector2(move * speed, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (grounded && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+        }
+    }
+}
