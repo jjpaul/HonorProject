28c167813645dd3effc4dca1e02474ec21a39e38
jjpaul <chun.k.pang@students.plymouth.ac.uk>
2016-02-23 19:28:55 +0000

    Forging Function
    Little optimization on character control, simple forging function
    without storage and dynamic item

---
+++ Assets/Script/ShowCase.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowCase : MonoBehaviour {
+
+    public GameObject waiting;
+    private GameObject waitingPrefab;
+    public GameObject showCase;
+    private GameObject showCasePf;
+
+    private bool called;
+    private bool canCall;
+    // Use this for initialization
+    void Start () {
+        called = false;
+        canCall = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (canCall && !called && Input.GetKeyDown(KeyCode.K))
+        {
+            showCasePf = Instantiate(showCase);
+            called = true;
+            canCall = false;
+        }
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("hi");
+            waitingPrefab = Instantiate(waiting, new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 1.5f), Quaternion.identity) as GameObject;
+            canCall = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("bye");
+            Destroy(waitingPrefab);
+            called = false;
+            Destroy(showCasePf);
+
+        }
+    }
+}
